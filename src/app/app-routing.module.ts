e16116e0f8770be1b637fdbc9bf48d2149621f50
vantercarlos <ra-1882123-5@alunos.unicesumar.edu.br>
2022-02-07 16:14:46 -0400

    Projeto POC

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,36 @@
+import { AuthGuard } from 'src/app/guards/auth.guard';
+import { NotLoggedGuard } from 'src/app/guards/not-logged.guard';
+import { HomePage } from 'src/app/pages/home/home.page';
+import { SignInPage } from 'src/app/pages/sign-in/sign-in.page';
+
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { MsalGuard } from '@azure/msal-angular';
+import { MicrosoftLoginGuard } from './services/microsoft-login.guard';
+
+const routes: Routes = [
+  { path: '', component: HomePage, canActivate: [MsalGuard] },
+  { path: 'sign-in', component: SignInPage, canActivate: [NotLoggedGuard] },
+
+  {
+    path: 'rsi',
+    loadChildren: () =>
+      import('projects/rsi/src/app/app.module').then(
+        (m) => m.AppRsiSharedModule
+      ),
+  },
+  {
+    path: 'ifd',
+    loadChildren: () =>
+      import('projects/ifd/src/app/app.module').then(
+        (m) => m.AppIfdSharedModule
+      ),
+  },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule {}
